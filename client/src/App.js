ae537918e3160bce03ace6e1ac912047c6a90c88
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-11-14 09:42:18 -0800

    add cors ans console log of courses

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,39 @@
 import logo from './logo.svg';
 import './App.css';
 
+let courses;
+
+async function getCourses() {
+  const response = await fetch("http://localhost:5000/api/courses")
+  courses = await response.json();
+  console.log(courses)
+}
+
 function App() {
+  getCourses()
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    // <div className="App">
+    //   <header className="App-header">
+    //     <img src={logo} className="App-logo" alt="logo" />
+    //     <p>
+    //       Edit <code>src/App.js</code> and save to reload.
+    //     </p>
+    //     <a
+    //       className="App-link"
+    //       href="https://reactjs.org"
+    //       target="_blank"
+    //       rel="noopener noreferrer"
+    //     >
+    //       Learn React
+    //     </a>
+    //   </header>
+    // </div>
+      <div>
+        <h1>Hi</h1>
+        <h3>Course titles</h3>
+
+      </div>
+
   );
 }
 
