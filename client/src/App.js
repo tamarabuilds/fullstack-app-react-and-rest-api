9fabdb6e3e772d232089a37de46cef5b0a7ed947
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-11-15 11:02:56 -0800

    simple fetch confirming connection to api

--- client/src/App.js
+++ client/src/App.js
@@ -1,36 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react'
 
-let courses;
 
-async function getCourses() {
-  const response = await fetch("http://localhost:5000/api/courses")
-  courses = await response.json();
-  console.log(courses)
-}
 
 function App() {
-  getCourses()
+  const [courses, setCourses] = useState(null)
+
+  useEffect(()=> {
+    console.log(`in fetch`)
+    fetch("http://localhost:5000/api/courses")
+      .then((response) => response.json())
+      .then((data) => {
+        setCourses(data);
+        console.log(data)
+      })
+  },[]);
+  
+  
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
       <div>
         <h1>Hi</h1>
         <h3>Course titles</h3>
+        <ul>
+          {courses.map( course => (<li>{course.id}: {course.title}</li>))}
+        </ul>
 
       </div>
 
