fd4a644b0bb61adc2cf3439a285df76bcd34fac5
Tamara Patterson <98510821+tamarabuilds@users.noreply.github.com>
2023-11-16 17:20:56 -0800

    add apiHelper and more UserContext

--- client/src/components/context/UserContext.js
+++ client/src/components/context/UserContext.js
@@ -1,18 +1,48 @@
 import { createContext, useState } from "react";
+import Cookies from 'js-cookie';
+import { api } from "../../utils/apiHelper";
 
 const UserContext = createContext(null);
 
 export const UserProvider = (props) => {
+    const cookie = Cookies.get('authenticatedUser');
+    const password = Cookies.get('authenticatedPassword')
+    const [authUser, setAuthUser] = useState(cookie ? JSON.parse(cookie) : null);
+    const [authPassword, setAuthPassword] = useState(password ? JSON.parse(password) : null);
 
     const signIn = async (credentials) => {
         console.log(`in signIn()`)
         console.log(credentials)
+
+        const response = await api('/users', 'GET', null, credentials);
+        console.log(response)
+        if (response.status === 200){
+            const user = await response.json();
+            setAuthUser(credentials.password);          // guessing here...
+            setAuthPassword(user);
+            Cookies.set("authenticatedUser", JSON.stringify(user), {expires: 1})
+            Cookies.set("authenticatedPassword", JSON.stringify(password), {expires: 1})
+            return user         // need to send password too?
+        } else if (response.status === 401) {
+            return null
+        } else {
+            throw new Error()
+        }
+    }
+
+    const signOut = () => {
+        setAuthUser(null);
+        setAuthPassword(null);
+        Cookies.remove('authenticatedUser');
+        Cookies.remove('authenticatedPassword');
     }
 
     return (
         <UserContext.Provider value={{
+            authUser,
             actions: {
-                signIn
+                signIn,
+                signOut
             }
         }}>
             {props.children}
---
+++ client/src/utils/apiHelper.js
@@ -0,0 +1,32 @@
+export const api = (
+    path,
+    method = "GET",
+    body = null,
+    credentials = null
+    ) => {
+    
+    const url = `http://localhost:5000/api` + path;
+
+    const options = {
+        method,
+        headers: {}
+    }
+
+    if (body) {
+        options.body = JSON.stringify(body);
+        // bracket notation needed because Content-Tyoe is a string
+        options.headers["Content-Type"] = "application/json; charset=utf-8";
+    }
+
+    // encoding user credentials following the basic authentication scheme requirement
+    // btoa() creates a base 64 endoded ASCII string from a string of data
+    // basic authentication requires the username and password to be separated by a colon :  
+    if (credentials) {
+        const encodedCredentials = btoa(`${credentials.emailAddress}:${credentials.password}`);
+
+        options.headers["Authorization"] = `Basic ${encodedCredentials}`
+    }
+
+    return fetch (url, options);
+
+};
